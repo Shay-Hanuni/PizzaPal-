b7d06608cb5f81afeaa6e25cfc572baaf3238710
Shay-Hanuni <shay258396@gmail.com>
2024-05-07 23:43:28 +0300

    Add files via upload

---
+++ MyDeliverySystem.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class MyDeliverySystem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MyDeliverySystem));
+            this.pic_pizzapeperonni = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pic_cheesy_pizza = new System.Windows.Forms.PictureBox();
+            this.pic_bavaria = new System.Windows.Forms.PictureBox();
+            this.pic_fanta = new System.Windows.Forms.PictureBox();
+            this.pic_coca_cola = new System.Windows.Forms.PictureBox();
+            this.pic_mus_shokolad = new System.Windows.Forms.PictureBox();
+            this.SumLabel = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_pizzapeperonni)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_cheesy_pizza)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_bavaria)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_fanta)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_coca_cola)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_mus_shokolad)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pic_pizzapeperonni
+            // 
+            this.pic_pizzapeperonni.BackColor = System.Drawing.Color.Transparent;
+            this.pic_pizzapeperonni.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pic_pizzapeperonni.BackgroundImage")));
+            this.pic_pizzapeperonni.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pic_pizzapeperonni.Location = new System.Drawing.Point(154, 38);
+            this.pic_pizzapeperonni.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pic_pizzapeperonni.Name = "pic_pizzapeperonni";
+            this.pic_pizzapeperonni.Size = new System.Drawing.Size(202, 167);
+            this.pic_pizzapeperonni.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic_pizzapeperonni.TabIndex = 1;
+            this.pic_pizzapeperonni.TabStop = false;
+            this.pic_pizzapeperonni.Click += new System.EventHandler(this.pic_pizzapeperonni_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(364, 12);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(171, 192);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            // 
+            // pic_cheesy_pizza
+            // 
+            this.pic_cheesy_pizza.BackColor = System.Drawing.Color.Transparent;
+            this.pic_cheesy_pizza.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pic_cheesy_pizza.BackgroundImage")));
+            this.pic_cheesy_pizza.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pic_cheesy_pizza.Location = new System.Drawing.Point(547, 45);
+            this.pic_cheesy_pizza.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pic_cheesy_pizza.Name = "pic_cheesy_pizza";
+            this.pic_cheesy_pizza.Size = new System.Drawing.Size(186, 148);
+            this.pic_cheesy_pizza.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic_cheesy_pizza.TabIndex = 3;
+            this.pic_cheesy_pizza.TabStop = false;
+            this.pic_cheesy_pizza.Click += new System.EventHandler(this.pic_cheesy_pizza_Click);
+            // 
+            // pic_bavaria
+            // 
+            this.pic_bavaria.BackColor = System.Drawing.Color.Transparent;
+            this.pic_bavaria.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pic_bavaria.BackgroundImage")));
+            this.pic_bavaria.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pic_bavaria.Location = new System.Drawing.Point(468, 233);
+            this.pic_bavaria.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pic_bavaria.Name = "pic_bavaria";
+            this.pic_bavaria.Size = new System.Drawing.Size(202, 167);
+            this.pic_bavaria.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic_bavaria.TabIndex = 4;
+            this.pic_bavaria.TabStop = false;
+            this.pic_bavaria.Click += new System.EventHandler(this.pic_bavaria_Click);
+            // 
+            // pic_fanta
+            // 
+            this.pic_fanta.BackColor = System.Drawing.Color.Transparent;
+            this.pic_fanta.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pic_fanta.BackgroundImage")));
+            this.pic_fanta.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pic_fanta.Location = new System.Drawing.Point(688, 196);
+            this.pic_fanta.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pic_fanta.Name = "pic_fanta";
+            this.pic_fanta.Size = new System.Drawing.Size(292, 231);
+            this.pic_fanta.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic_fanta.TabIndex = 5;
+            this.pic_fanta.TabStop = false;
+            this.pic_fanta.Click += new System.EventHandler(this.pic_fanta_Click);
+            // 
+            // pic_coca_cola
+            // 
+            this.pic_coca_cola.BackColor = System.Drawing.Color.Transparent;
+            this.pic_coca_cola.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pic_coca_cola.BackgroundImage")));
+            this.pic_coca_cola.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pic_coca_cola.Location = new System.Drawing.Point(741, 38);
+            this.pic_coca_cola.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pic_coca_cola.Name = "pic_coca_cola";
+            this.pic_coca_cola.Size = new System.Drawing.Size(202, 167);
+            this.pic_coca_cola.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic_coca_cola.TabIndex = 6;
+            this.pic_coca_cola.TabStop = false;
+            this.pic_coca_cola.Click += new System.EventHandler(this.pic_coca_cola_Click);
+            // 
+            // pic_mus_shokolad
+            // 
+            this.pic_mus_shokolad.BackColor = System.Drawing.Color.Transparent;
+            this.pic_mus_shokolad.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pic_mus_shokolad.BackgroundImage")));
+            this.pic_mus_shokolad.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pic_mus_shokolad.Location = new System.Drawing.Point(307, 250);
+            this.pic_mus_shokolad.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pic_mus_shokolad.Name = "pic_mus_shokolad";
+            this.pic_mus_shokolad.Size = new System.Drawing.Size(154, 112);
+            this.pic_mus_shokolad.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic_mus_shokolad.TabIndex = 7;
+            this.pic_mus_shokolad.TabStop = false;
+            this.pic_mus_shokolad.Click += new System.EventHandler(this.pic_mus_shokolad_Click);
+            // 
+            // SumLabel
+            // 
+            this.SumLabel.AutoSize = true;
+            this.SumLabel.BackColor = System.Drawing.Color.Transparent;
+            this.SumLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.SumLabel.Location = new System.Drawing.Point(1013, 460);
+            this.SumLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.SumLabel.Name = "SumLabel";
+            this.SumLabel.Size = new System.Drawing.Size(36, 37);
+            this.SumLabel.TabIndex = 8;
+            this.SumLabel.Text = "$";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.ErrorImage = null;
+            this.pictureBox2.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.InitialImage")));
+            this.pictureBox2.Location = new System.Drawing.Point(736, 451);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(258, 58);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Chartreuse;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label1.Location = new System.Drawing.Point(362, 387);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "20.00$";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Chartreuse;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label2.Location = new System.Drawing.Point(557, 387);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 20);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "20.00$";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Chartreuse;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label3.Location = new System.Drawing.Point(246, 213);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 20);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "55.00$";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Chartreuse;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label4.Location = new System.Drawing.Point(434, 213);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 20);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "35.00$";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Chartreuse;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label5.Location = new System.Drawing.Point(621, 213);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 20);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "30.00$";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Chartreuse;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label6.Location = new System.Drawing.Point(816, 213);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(54, 20);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "10.00";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Chartreuse;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label7.Location = new System.Drawing.Point(816, 387);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(64, 20);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "10.00$";
+            // 
+            // MyDeliverySystem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1140, 556);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.SumLabel);
+            this.Controls.Add(this.pic_mus_shokolad);
+            this.Controls.Add(this.pic_coca_cola);
+            this.Controls.Add(this.pic_fanta);
+            this.Controls.Add(this.pic_bavaria);
+            this.Controls.Add(this.pic_cheesy_pizza);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pic_pizzapeperonni);
+            this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "MyDeliverySystem";
+            this.Text = "MyDeliverySystem";
+            ((System.ComponentModel.ISupportInitialize)(this.pic_pizzapeperonni)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_cheesy_pizza)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_bavaria)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_fanta)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_coca_cola)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_mus_shokolad)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pic_pizzapeperonni;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pic_cheesy_pizza;
+        private System.Windows.Forms.PictureBox pic_bavaria;
+        private System.Windows.Forms.PictureBox pic_fanta;
+        private System.Windows.Forms.PictureBox pic_coca_cola;
+        private System.Windows.Forms.PictureBox pic_mus_shokolad;
+        private System.Windows.Forms.Label SumLabel;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
